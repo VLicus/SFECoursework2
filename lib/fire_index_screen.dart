671f29f186c3a278ed21e1b973700ce5cf823998
EFolanBell <99076514+EFolanBell@users.noreply.github.com>
2022-05-11 17:54:29 +0100

    Update fire_index_screen.dart

--- lib/fire_index_screen.dart
+++ lib/fire_index_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sfe_cw2_api_tester/home_screen.dart';
+import 'package:sfe_cw2_api_tester/weather_screen.dart';
 
 class FireIndex extends StatefulWidget {
   @override
@@ -43,12 +44,78 @@ class InitState extends State<FireIndex> {
                 margin: EdgeInsets.only(right: 20, top: 20),
                 alignment: Alignment.bottomRight,
                 child: Text(
-                  "Fire Index Calculator",
+                  "Fire Risk Index Calculator",
                   style: TextStyle(fontSize: 25, color: Colors.white),
                 ),
               )
             ],
           )),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ));
+          },
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            height: 100,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [(Colors.green), Colors.green],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight),
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.grey[200],
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: Color(0xffEEEEEE)),
+              ],
+            ),
+            child: Text(
+              "FSI Level 1 = Low Fire Severity, FSI Level 2 = Moderate Fire Severity, FSI Level 3 = High Fire Severity, FSI Level 4 = Very High Fire Severity ",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ));
+          },
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            height: 280,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                (Color.fromARGB(255, 90, 202, 94)),
+                Color.fromARGB(255, 76, 197, 81)
+              ], begin: Alignment.centerLeft, end: Alignment.centerRight),
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.grey[200],
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: Color(0xffEEEEEE)),
+              ],
+            ),
+            child: Text(
+              "The fire risk index in your area is value : Level 1  ",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
         Container(
           margin: EdgeInsets.only(top: 3, bottom: 10),
@@ -59,7 +126,7 @@ class InitState extends State<FireIndex> {
               GestureDetector(
                 child: Text(
                   "Back to Home page",
-                  style: TextStyle(color: Color.fromARGB(255, 31, 45, 245)),
+                  style: TextStyle(color: Color.fromARGB(255, 12, 163, 20)),
                 ),
                 onTap: () {
                   // Write Tap Code Here.
